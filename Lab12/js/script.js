2572c1ff13b44c56a40f6728664d0bc1b289772e
Emily Zielinski <zieli1eh@cmich.edu>
2026-04-21 12:25:10 -0400

    changes

---
+++ Lab12/js/script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript is connected!");
+
+// Open the overlay
+function openOverlay() {
+    document.getElementById("overlay-menu").style.width = "100vw";
+}
+
+// Close the overlay
+function closeOverlay() {
+    document.getElementById("overlay-menu").style.width = "0";
+}
